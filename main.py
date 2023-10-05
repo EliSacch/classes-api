bc8823a73ad29b0a644cd589e4ce7dc0e6a4fde2
elisacch <elisa.sacchelli@icloud.com>
2023-10-05 15:58:12 +0100

    Add functionality to post new classes and save data in json file

--- main.py
+++ main.py
@@ -5,46 +5,77 @@ import json
 app = Flask(__name__)
 
 
+def get_data():
+    """
+    This function is used to get data from data.json file.
+    It opens the file and handle exceptions in case data cannot be retrieved.
+    """
+    try:
+        f = open("data.json")
+        data = json.load(f)
+        f.close()
+        return data
+        
+    except FileNotFoundError:
+        # This exception checks if the file exists
+        return {"error": "This file doesn't exist."}
+    except Exception:
+        # To handle all other exceptions
+        return {"error": "There was a problem retrieving the data from this file."}
+
+
 @app.route("/")
 def home():
-    """This is the home"""
+    """ This is the home """
     return {"message": "Welcome!"}
 
 
 @app.route("/classes", methods=["POST", "GET"])
 def classes():
-    """Classes endpoint, to show existig classes
+    """
+    Classes endpoint, to show existig classes
     and create new ones.
-    The classes are saved in a file called data.json"""
+    The classes are saved in a file called data.json
+    """
     if request.method == "POST":
-        # get posted information
+        # Get posted information
         new_class = request.json
         id = new_class['id']
 
-        # get existing data from data.json
-        f = open("data.json")
-        data = json.load(f)
-        gym_classes = data["gym_classes"]
+        # Get existing data from data.json
+        data = get_data()
 
-        # add new class to existing classes
-        gym_classes[f"{id}"] = new_class
+        # If there is an error in the file, do not submit the new data
+        if "error" in data.keys():
+            return data
+        
+        else:
+            # first check that "all_classes exist", or create it
+            if "all_classes" not in data.keys():
+                data['all_classes'] = {}
 
-        # overwrite data.json file to include new data
-        with open("data.json", "w") as f:
-            json.dump(data, f)
-            f.close()
-        # return the newly added class with rsponse of 201
-        return new_class, 201
+            # Then add the new_class
+            data['all_classes'][f"{id}"] = new_class
+            # Overwrite data.json file to include new data
+            with open("data.json", "w") as f:
+                json.dump(data, f)
+                f.close()
+
+            # return the newly added class with rsponse of 201
+            return new_class, 201
     
     else:
         # if the method is not POST, we just get existing classes
-        f = open("data.json")
-        data = json.load(f)
-        gym_classes = data["gym_classes"]
-        f.close()
-        # If there is no existing class we return a message
-        return gym_classes if gym_classes != {} else {"message": "There are no classes yet"}, 200
-    
+        data = get_data()
+        if "all_classes" in data.keys() and data['all_classes'] != {}:
+            return data['all_classes']
+        # if there is an error getting the data from the file, we return the error
+        elif "error" in data.keys():
+            return data
+        # if there is no error, but there is no key called 'all_classes', we return a message
+        else:
+            return {"message": "There are no classes to display."}
+        
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import requests
 
 BASE = "http://127.0.0.1:8000/"
 
-data={"id": 1, "class_name": "Pilates", "capacity": 10, "start_date": "01-10-2023", "end_date": "31-10-2023"}
-response = requests.post("http://127.0.0.1:8000/classes", json=data)
+data={"id": 0, "class_name": "Pilates", "capacity": 10, "start_date": "01-10-2023", "end_date": "31-10-2023"}
+response = requests.post(BASE + "/classes", json=data)
 print(response.json())
