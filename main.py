823511c3b1248161801a919e668eaeb871e97517
elisacch <elisa.sacchelli@icloud.com>
2023-10-05 12:36:29 +0100

    Create classes route with GET and POST methods

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 from flask import Flask, request, jsonify
+import json
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return {"data": "Welcome!"}
+    """This is the home"""
+    return {"message": "Welcome!"}
 
+
+@app.route("/classes", methods=["POST", "GET"])
+def classes():
+    """Classes endpoint, to show existig classes
+    and create new ones.
+    The classes are saved in a file called data.json"""
+    if request.method == "POST":
+        # get posted information
+        new_class = request.json
+        id = new_class['id']
+
+        # get existing data from data.json
+        f = open("data.json")
+        data = json.load(f)
+        gym_classes = data["gym_classes"]
+
+        # add new class to existing classes
+        gym_classes[f"{id}"] = new_class
+
+        # overwrite data.json file to include new data
+        with open("data.json", "w") as f:
+            json.dump(data, f)
+            f.close()
+        # return the newly added class with rsponse of 201
+        return new_class, 201
+    
+    else:
+        # if the method is not POST, we just get existing classes
+        f = open("data.json")
+        data = json.load(f)
+        gym_classes = data["gym_classes"]
+        f.close()
+        # If there is no existing class we return a message
+        return gym_classes if gym_classes != {} else {"message": "There are no classes yet"}, 200
+    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import requests
 
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://127.0.0.1:8000/"
 
-
-response = requests.get(BASE + "")
+data={"id": 1, "class_name": "Pilates", "capacity": 10, "start_date": "01-10-2023", "end_date": "31-10-2023"}
+response = requests.post("http://127.0.0.1:8000/classes", json=data)
 print(response.json())
